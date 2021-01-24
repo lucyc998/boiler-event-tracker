fc23cf168304806035d0e2a27d993d00429559cc
Jude Adham <judeadham@gmail.com>
2021-01-24 01:14:31 -0500

    added code for radar API calls

---
+++ radarAPIcalls.py
@@ -0,0 +1,27 @@
+import requests
+import json
+
+
+def getNearestLocation(buildingName):
+    # coordinates for Purdue University
+    coords = '40.423538,-86.9217'
+    restParams = {'query': buildingName, 'near': coords}
+    authHeader = {'Authorization': 'prj_test_pk_52f1b430dab9397c36c08527cf7957bbdf4bd9e6'}
+    radarResponse = requests.get('https://api.radar.io/v1/search/autocomplete', headers=authHeader, params=restParams)
+    return radarResponse
+
+
+if __name__ == "__main__":
+    userBuilding = input("Enter a building name: ")
+    response = getNearestLocation(userBuilding)
+    if response.status_code != 200:
+        print("Error! Something went wrong!")
+    else:
+        locList = response.json()['addresses']
+        print(json.dumps(response.json(), indent=4))
+        for loc in locList:
+            if 'placeLabel' in loc.keys():
+                print(f"The address for \"{userBuilding}\" is at {loc['formattedAddress']}, it is also known as: {loc['placeLabel']}.")
+            else:
+                print(f"The address for \"{userBuilding}\" is at {loc['formattedAddress']}, We couldn't quite find a label for it though!")
+            print(f"It is located at a latitude of {loc['latitude']} and a longitude of {loc['longitude']}.\n")
