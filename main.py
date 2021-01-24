54c4048f8bd43b4ff638f01abc6c19d915f78515
Justin <54717842+justinselbo@users.noreply.github.com>
2021-01-24 07:21:38 -0500

    modified:   main.py

--- main.py
+++ main.py
@@ -2,112 +2,146 @@ import kivy
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.label import Label
-from kivy_garden.mapview import MapView, MapMarker
+from kivy_garden.mapview import MapView, MapMarkerPopup
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
+from kivy.core.window import Window
+from kivy.uix.button import Button 
+from radarAPIcalls import getAddresses
+from kivy.uix.popup import Popup
+from kivy.uix.gridlayout import GridLayout 
+from database import uploadToDB
 from database import fetchFromDB
-#from database import uploadToDB
+
+Window.size = (350, 625)
 
 Builder.load_string("""
-<MenuScreen>:
-    FloatLayout:
-        Button:
-            text: 'Goto map'
-            size_hint: (.3, .2)
-            on_press: root.manager.current = 'map'
-            pos: (75,200)
-        Button:
-            text: 'Quit'
-            size_hint: (.3, .2)
-            pos: (400, 200)
-
 <MapScreen>:
     FloatLayout:
         MapView:
             id: map_view
             lat: 40.423538
             lon: -86.9217
-            zoom: 18
-            on_touch_down: print("nice")
-            MapMarkerPopup:
-                lat: 40.423538
-                lon: -86.9217
+            zoom: 15
         Button:
-            text: 'Add an event'
+            text: 'Add Event'
             on_press: root.manager.current = 'event'
-            size_hint: (.3, .2)
-            pos: (600, 100)
-        Button:
-            text: 'Back to menu'
-            on_press: root.manager.current = 'menu'
-            size_hint: (.3, .2)
-            pos: (600, 0)
+            size_hint: (.3, .12)
+            pos: (125, 20)
+        Label:
+            text: 'Boiler Event Tracker'
+            pos: (0, 297)
+            font_size: 20
+            color: (0, 0, 0, 1)
 
 <EventScreen>:
-    GridLayout:
-        cols:1
-        spacing:200
-        size: root.width-300, root.height-300
-        GridLayout:
-            spacing:20
-            
-            cols:2
-            Label:
-                text: "Event Name:"
-            TextInput:
-                id: name_box
-                multiline:False
-            Label:
-                text: "Description:"
-            TextInput:
-                id: descrip_box
-        GridLayout:
-            cols:2
-            Button:
-                text: "Add Event"
-                on_press: root.manager.current = 'map'
-            Button:
-                text: "Cancel"
-                on_press: root.manager.current = 'map'                
+    FloatLayout:
+        Button:
+            text: 'Back'
+            on_press: root.manager.current = 'map'
+            size_hint: (.43, .12)
+            pos: (17, 20)
+        Button:
+            text: 'Upload'
+            on_press: root.upload(name_box.text, descrip_box.text, location_box.text)
+            size_hint: (.43, .12)
+            pos: (183, 20)
+        TextInput
+            text: 'Event Name: '
+            id: name_box
+            font_size: 25
+            size_hint: (.90, .24)
+            pos: (17, 455)
+            background_color: (255, 255, 255, 1)
+            color: (255, 255, 255, 1)
+            multiline: True 
+        TextInput
+            text: 'Location: '
+            id: location_box 
+            font_size: 25
+            size_hint: (.90, .24)
+            pos: (17, 285)
+            background_color: (255, 255, 255, 1)
+            color: (255, 255, 255, 1)
+            multiline: True
+        TextInput
+            text: 'Description: '
+            id: descrip_box 
+            font_size: 25
+            size_hint: (.90, .24)
+            pos: (17, 115)
+            background_color: (255, 255, 255, 1)
+            color: (255, 255, 255, 1)
+            multiline: True 
 """)
 
+
 # Declare screens
-class MenuScreen(Screen):
-    pass
 
 class MapScreen(Screen):
-    def on_touch_down(self,touch):
-        #super().on_touch_down(touch)
-        print(touch)
-        print(type(touch.pos))
-        print(touch.pos[1])
-        print(self.ids.map_view.lon)
-        self.ids.map_view.add_marker(MapMarker(lon=self.ids.map_view.lon, lat=self.ids.map_view.lat, source='map_marker.png'))
+
+    def on_touch_down(self, touch):
+        super().on_touch_down(touch)
+        posts = fetchFromDB()
+        for event in posts:
+            lat = event['lat']
+            lon = event['lon']
+            marker = MapMarkerPopup(lon=lon,lat=lat, source="small_marker.png")
+            self.ids.map_view.add_marker(marker)
+            btn = Button(
+                text ='Event: ' + event['Event'] + '\n\n\n' + 'Description:' + event['Description'],
+                size_hint = (3, 2),
+                font_size = 20)
+            btn.bind(on_release= callback)
+            marker.add_widget(btn)
+
+        
+def callback(instance):
+    instance.opacity=0
+    instance.disabled=True
+ 
 
 class EventScreen(Screen):
-    pass
+    def upload(self, title, descrip, location):
+        #do database things
+        result = getAddresses(location.replace('Location:', "").strip())[0]
+        if result == 0:
+            return 0
+        uploadToDB(title.replace("Event Name:", ""), result['placeLabel'], descrip.replace("Description:", ""), result['latitude'], result['longitude'])
+        #print('Latitude = {0}, Longitude = {1}, Title = {2}, Description = {3}'.format(lat, long, title, descrip, location))
+
+
 
 class Touch(Widget):
-    def on_touch_down(self,touch):
+    def on_touch_down(self, touch):
         super().on_touch_down(touch)
-        print(touch)
-        print("hello")
+
 
 class MyApp(App):
     def build(self):
         # Create the screen manager
         sm = ScreenManager()
-        sm.add_widget(MenuScreen(name='menu'))
         sm.add_widget(MapScreen(name='map'))
         sm.add_widget(EventScreen(name='event'))
-        
-        return sm
 
-print(fetchFromDB()[0])
+        return sm
 
 if __name__ == '__main__':
     MyApp().run()
-#MyApp().run()
+# MyApp().run()
 
-
+''' m1 = MapMarkerPopup(lon=-86.9217, lat=40.423538, source="small_marker.png")
+        self.ids.map_view.add_marker(m1)
+        btn1 = Button(text="Get event info")
+        btn1.bind(on_press=callback)
+        m1.add_widget(btn1)'''
+       
+''' def add_markers():
+        posts = fetchFromDB()
+        for event in posts:
+            print(event)
+            lat = event['lat']
+            lon = event['lon']
+            marker = MapMarkerPopup(lon=lon,lat=lat, source="small_marker.png")
+            self.ids.map_view.add_marker(marker)  '''
